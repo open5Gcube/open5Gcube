93c97f410b4ca824479c0cb5825b563afa9aa8e6
Thorsten Horstmann <Thorsten.Horstmann@fkie.fraunhofer.de>
2026-02-27 05:48:07 +0100

    Removed docker privileged mode from o5gc webui (refs #14)

--- modules/base/docker/o5gc/webui/entrypoint.sh
+++ modules/base/docker/o5gc/webui/entrypoint.sh
@@ -4,17 +4,14 @@ set -ex
 
 envsubst.sh /mnt/webui/nginx.conf /etc/nginx/nginx.conf
 
-nginx -t
+service nginx configtest
 service nginx start
 
 if [ -d /mnt/.ssh/ ]; then
     cp -a /mnt/.ssh/* ~/.ssh
     chown $(id -u):$(id -g) ~/.ssh/*
-    rm -f ~/.ssh/known_hosts
     echo -e "Host *\n\tUser ${HOST_USER}" >> ~/.ssh/config
 fi
-[ "${ENB_HOSTNAME}" != "localhost" ] && ssh-keyscan ${ENB_HOSTNAME} >> ~/.ssh/known_hosts
-[ "${GNB_HOSTNAME}" != "localhost" ] && ssh-keyscan ${GNB_HOSTNAME} >> ~/.ssh/known_hosts
 
 export USER=${HOST_USER}
 export HOME=${HOST_HOME}
