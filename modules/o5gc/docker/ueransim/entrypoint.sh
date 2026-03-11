f9b9c0cae590c4063ccfe3989e8d830bcdb50d02
Thorsten Horstmann <Thorsten.Horstmann@fkie.fraunhofer.de>
2026-03-11 14:09:56 +0100

    UERANSIM gnb waits for open N2 SCTP port of AMF

--- modules/o5gc/docker/ueransim/entrypoint.sh
+++ modules/o5gc/docker/ueransim/entrypoint.sh
@@ -9,7 +9,7 @@ if [[ -z "${service}" ]]; then
     exit 1;
 fi
 
-[[ "${service}" == "gnb" ]] && wait-for-it -t 60 ${AMF_IP_ADDR}:7777
+[[ "${service}" == "gnb" ]] && retry --until=success --times=10 --delay=5 -- ncat -z --sctp ${AMF_IP_ADDR} 38412
 [[ "${service}" == "ue" ]] && retry --until=success --times=10 --delay=5 -- nc -v -z -u ${GNB_RF_IP_ADDR} 4997
 
 [ "${NSSAI_SD}" == "ffffff" ] && export NSSAI_SD=0x${NSSAI_SD}
